784fcd1ea7d3105711fcec0d1ca041ed03c920a1
afnan267 <m.afnanakber@gmail.com>
2025-02-22 12:48:23 +0500

    feat: added dummy images for x-rays

--- src/Assets/Images/index.ts
+++ src/Assets/Images/index.ts
@@ -2,4 +2,8 @@ export const images = {
     google: require('../Images/google_image.png'),
     apple: require('../Images/apple.png'),
     teethImage: require('../Images/teeth_image.png'),
+    xRayImage1: require('../Images/x_ray.png'),
+    xRayImage2: require('../Images/x_ray_2.png'),
+    xRayImage3: require('../Images/x_ray_3.png'),
+
    };
