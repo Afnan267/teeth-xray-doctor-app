69c52330673c4de098345ffb6b4b16cc51ca4cb6
afnan267 <m.afnanakber@gmail.com>
2025-02-27 23:17:40 +0500

    feat: GestureHandlerRootView shifting from registration screen to app.tsx

--- src/screens/Auth/Registration/RegistrationScreen.tsx
+++ src/screens/Auth/Registration/RegistrationScreen.tsx
@@ -16,7 +16,7 @@ import TermsCheckbox from '../../../components/TermsCheckbox';
 import SocialAuthButtons from '../../../components/SocialAuthButtons';
 import {GestureHandlerRootView, ScrollView} from 'react-native-gesture-handler';
 import Spacer from '../../../Elements/Spacer';
-import { useAuth } from '../../../hooks/useAuth';
+import {useAuth} from '../../../hooks/useAuth';
 
 type RegistrationProps = NativeStackScreenProps<
   RootStackParamList,
@@ -35,14 +35,13 @@ const RegistrationScreen = ({navigation}: RegistrationProps) => {
 
   const [isSelected, setIsSelected] = useState(false);
 
-  const { user, loading, signInWithGoogle } = useAuth();
+  const {user, loading, signInWithGoogle} = useAuth();
 
   const onGoogleSignPress = async () => {
     const userData = await signInWithGoogle();
     if (userData) {
       console.log('Signed in user:', userData);
       navigation.navigate('HomeScreen');
-      // You can now store user globally (e.g., Redux) or navigate
     }
   };
 
@@ -66,77 +65,74 @@ const RegistrationScreen = ({navigation}: RegistrationProps) => {
 
   const onAppleSignPress = () => {};
 
-
   return (
-    <GestureHandlerRootView style={{flex: 1, backgroundColor: '#f9f9ff'}}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-        style={styles.container}>
-        <ScrollView
-          contentContainerStyle={{flexGrow: 1, paddingBottom: 40}}
-          keyboardShouldPersistTaps="handled"
-          showsVerticalScrollIndicator={false}>
-          <Text style={styles.nameTitle}>Create an account</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{flexGrow: 1, paddingBottom: 40}}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}>
+        <Text style={styles.nameTitle}>Create an account</Text>
 
-          <View style={styles.loginContainer}>
-            <Text style={styles.nameSubTitle}>Already have an account? </Text>
-            <Pressable onPress={() => navigation.navigate('Login')}>
-              <Text style={styles.logInTitle}>Login</Text>
-            </Pressable>
-          </View>
-          <Spacer height={66} />
-          <InputBox
-            placeholder="First Name"
-            value={form.firstName}
-            onChangeText={value => handleChange('firstName', value)}
-            errorMessage={errors.firstName}
-          />
-          <Spacer height={10} />
-          <InputBox
-            placeholder="Last Name"
-            value={form.lastName}
-            onChangeText={value => handleChange('lastName', value)}
-            errorMessage={errors.lastName}
-          />
+        <View style={styles.loginContainer}>
+          <Text style={styles.nameSubTitle}>Already have an account? </Text>
+          <Pressable onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.logInTitle}>Login</Text>
+          </Pressable>
+        </View>
+        <Spacer height={66} />
+        <InputBox
+          placeholder="First Name"
+          value={form.firstName}
+          onChangeText={value => handleChange('firstName', value)}
+          errorMessage={errors.firstName}
+        />
+        <Spacer height={10} />
+        <InputBox
+          placeholder="Last Name"
+          value={form.lastName}
+          onChangeText={value => handleChange('lastName', value)}
+          errorMessage={errors.lastName}
+        />
 
-          <Spacer height={10} />
-          <InputBox
-            placeholder="Email"
-            value={form.email}
-            onChangeText={value => handleChange('email', value)}
-            errorMessage={errors.email}
-          />
-          <Spacer height={10} />
-          <InputBox
-            placeholder="Enter your password"
-            value={form.password}
-            isPassword={true} // Pass as password field
-            onTogglePassword={() => setShowPassword(!showPassword)} // Toggle function
-            secureTextEntry={!showPassword}
-            onChangeText={value => handleChange('password', value)}
-            errorMessage={errors.password}
-          />
+        <Spacer height={10} />
+        <InputBox
+          placeholder="Email"
+          value={form.email}
+          onChangeText={value => handleChange('email', value)}
+          errorMessage={errors.email}
+        />
+        <Spacer height={10} />
+        <InputBox
+          placeholder="Enter your password"
+          value={form.password}
+          isPassword={true} // Pass as password field
+          onTogglePassword={() => setShowPassword(!showPassword)} // Toggle function
+          secureTextEntry={!showPassword}
+          onChangeText={value => handleChange('password', value)}
+          errorMessage={errors.password}
+        />
 
-          <TermsCheckbox
-            isSelected={isSelected}
-            onToggle={toggleSelection}
-            errorMessage={errors.terms}
-          />
-          <Spacer height={44} />
-          <Button
-            text="Create Account"
-            onPress={handlePress}
-            textColor="#FFFFFF"></Button>
+        <TermsCheckbox
+          isSelected={isSelected}
+          onToggle={toggleSelection}
+          errorMessage={errors.terms}
+        />
+        <Spacer height={44} />
+        <Button
+          text="Create Account"
+          onPress={handlePress}
+          textColor="#FFFFFF"></Button>
 
-          <Text style={styles.orText}>Or</Text>
+        <Text style={styles.orText}>Or</Text>
 
-          <SocialAuthButtons
-            onGooglePress={() => onGoogleSignPress()}
-            onApplePress={() => onAppleSignPress()}
-          />
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </GestureHandlerRootView>
+        <SocialAuthButtons
+          onGooglePress={() => onGoogleSignPress()}
+          onApplePress={() => onAppleSignPress()}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
